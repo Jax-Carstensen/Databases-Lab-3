6b02c95ebecc80bf85f1b1432ce0e00388a2aaf3
Jax-Carstensen <39423374+Jax-Carstensen@users.noreply.github.com>
2021-06-25 15:30:10 -0500

    Update Lab-3.sql

--- Lab-3.sql
+++ Lab-3.sql
@@ -66,6 +66,20 @@ create table StudentAdvisors(
 	foreign key(StudentSSN) references Students(SSN),
 	foreign key(StudentSSN) references Students(SSN)
 );
+create table ResearchAssistants(
+	StudentSSN varchar(10),
+	ProjNumber int,
+	foreign key(StudentSSN) references Students(SSN),
+	foreign key(ProjNumber) references Projects(ProjectNumber)
+)
+insert into Students values('12345678', 'Computer Science', '02/05/2001', 'John Mayor');
+insert into Students values('23456789', 'Math', '03/07/2002', 'Earl Diggins');
+insert into Students values('23456781', 'Computer Science', '02/05/2001', 'Nathan Digby');
+insert into Students values('62346723', 'Science', '10/23/1996', 'Madison Smith');
+insert into Students values('979267592', 'Math', '01/27/2004', 'Daniel Digby');
+insert into Students values('236714723', 'Science', '07/16/1999', 'Ethan Nelson');
+insert into Students values('834357372', 'Computer Science', '05/30/2000', 'Jacob Johnson');
+select * from Students s where s.DOB >= '01/01/2001';
 select * from WorksIn
 /*
 What is the total budget for all projects?
